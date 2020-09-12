ca1bbf58c8760f8923c504e460b6d5d284e8ca7c
Alfaxad <Alfaxadeyembe@gmail.com>
2020-09-12 19:34:01 +0000

    fixes

--- 0x06-regular_expressions/100-textme.rb
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env ruby
-SENDER =  ARGV[0].scan(/(?<=\[from:)[^ ]+(?=\])/).join
-RECIEVER =  ARGV[0].scan(/(?<=\[to:)[^ ]+(?=\])/).join
-FLAGS =  ARGV[0].scan(/(?<=\[flags:)[^ ]+(?=\])/).join
-puts "#{SENDER},#{RECIEVER},#{FLAGS}"
